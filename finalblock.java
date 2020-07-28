2574f8438734aa4b910f00ee1e6a8797a6d1e25d
Satyanarayan Bandha <satyanarayanbandha3@gmail.com>
2020-07-28 11:39:59 +0530

    new block

---
+++ finalblock.java
@@ -0,0 +1,20 @@
+try
+{
+   // Protected code
+}
+catch (ExceptionType1 e1)
+{
+   // Catch block
+}
+catch (ExceptionType2 e2)
+{
+   // Catch block
+}
+catch (ExceptionType3 e3)
+{
+   // Catch block
+}
+finally
+{
+   // The finally block always executes.
+}
